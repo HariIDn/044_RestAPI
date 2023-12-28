310e2d8d9f52fead8c0dfd3eb2e28e872676c785
HariIDn <aldifaadit75@gmail.com>
2023-12-28 17:24:06 +0700

    Membuat KontakApp pada package theme

---
+++ app/src/main/java/com/example/kelas10/ui/theme/KontakApp.kt
@@ -0,0 +1,60 @@
+package com.example.kelas10.ui.theme
+
+import android.os.Build
+import androidx.annotation.RequiresExtension
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.kelas10.R
+import com.example.kelas10.ui.PenyediaViewModel
+import com.example.kelas10.ui.home.screen.HomeScreen
+import com.example.kelas10.ui.home.viewmodel.HomeViewModel
+
+@RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun KontakApp(
+    homeViewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
+
+) {
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold(
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = { TopAppBarKontak(title = "Judul") }
+    ) {
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it)
+        ) {
+            HomeScreen(
+                kontakUIState = homeViewModel.kontakUIState,
+                retryAction = homeViewModel::getKontak
+            )
+
+
+        }
+    }
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopAppBarKontak(
+    title: String,
+
+){
+
+}
