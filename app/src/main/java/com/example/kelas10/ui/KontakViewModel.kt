0fad89d123ae789a2773e1bf752c3a893d062e1e
HariIDn <aldifaadit75@gmail.com>
2023-12-28 17:24:47 +0700

    Membuat KontakViewModel

---
+++ app/src/main/java/com/example/kelas10/ui/KontakViewModel.kt
@@ -0,0 +1,25 @@
+package com.example.kelas10.ui
+
+import android.os.Build
+import android.text.Spannable.Factory
+import androidx.annotation.RequiresExtension
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.kelas10.KontakApplication
+import com.example.kelas10.ui.home.viewmodel.HomeViewModel
+
+object PenyediaViewModel{
+
+    @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(aplikasiKontak().container.kontakRepository)
+        }
+    }
+}
+
+fun CreationExtras.aplikasiKontak(): KontakApplication =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as KontakApplication)
